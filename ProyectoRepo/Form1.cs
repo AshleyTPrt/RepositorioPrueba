65e47fb8b55f72e7ff4bfabab1062dbe44ddf691
Ashley Valverde <tvalverdeashley@gmail.com>
2024-04-19 08:27:44 -0600

    Comentario hecho

--- ProyectoRepo/Form1.cs
+++ ProyectoRepo/Form1.cs
@@ -6,5 +6,10 @@ namespace ProyectoRepo
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Comentario
+        }
     }
 }
